a26d730ee68f3cba2590467dfe3d50c7e29def5c
Nataren, Emiliano (GLOBAL_MX) <emiliano.nataren@mcd.mxc.com>
2026-06-25 11:32:35 -0600

    deleting innecesary configuration

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,24 +1,22 @@
--- This file needs to have same structure as nvconfig.lua 
--- https://github.com/NvChad/ui/blob/v3.0/lua/nvconfig.lua
--- Please read that file to know all available options :( 
-
 ---@type ChadrcConfig
 local M = {}
 
 M.base46 = {
-	theme = "tokyonight",
-
-	-- hl_override = {
-	-- 	Comment = { italic = true },
-	-- 	["@comment"] = { italic = true },
-	-- },
+  theme = "tokyonight",
 }
 
--- M.nvdash = { load_on_startup = true }
--- M.ui = {
---       tabufline = {
---          lazyload = false
---      }
--- }
+M.nvdash = {
+  load_on_startup = true,
+  header = {
+    "███╗   ██╗██╗   ██╗██╗███╗   ███╗",
+    "████╗  ██║██║   ██║██║████╗ ████║",
+    "██╔██╗ ██║██║   ██║██║██╔████╔██║",
+    "██║╚██╗██║╚██╗ ██╔╝██║██║╚██╔╝██║",
+    "██║ ╚████║ ╚████╔╝ ██║██║ ╚═╝ ██║",
+    "╚═╝  ╚═══╝  ╚═══╝  ╚═╝╚═╝     ╚═╝",
+    "",
+    "⚡ Welcome back Emiliano",
+  },
+}
 
 return M
